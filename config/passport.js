69b43436c0d4c455ae367a04161a13e5b6430836
Nuntius Creative Hub <nuntius.creative.hub@gmail.com>
2025-11-25 10:04:53 -0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,8 @@ const { findUserByEmail, createUser, updateUserPicture, getDB } = require('../da
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    proxy: true
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
@@ -48,5 +49,6 @@ passport.deserializeUser(async (id, done) => {
     done(error, null);
   }
 });
+
 
 module.exports = passport;
